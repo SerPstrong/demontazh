2094c973623f28bc0ecfe235927eae99841fad00
Sergey Plotnikov <ommuts@gmail.com>
2020-02-26 19:11:33 +0500

    del

--- js/demontazh.js
+++ js/demontazh.js
@@ -1,22 +1,21 @@
 (function () {
-  var app = "https://script.google.com/macros/s/AKfycbxOw6kKxWKL5hBucqTtrTPWtPHnYnx8zkvk52ZbgxJnlQb_nio/exec",
+  let app = "https://script.google.com/macros/s/AKfycbxOw6kKxWKL5hBucqTtrTPWtPHnYnx8zkvk52ZbgxJnlQb_nio/exec",
     output = '',
     xhr = new XMLHttpRequest();
   xhr.open('GET', app);
   xhr.onreadystatechange = function () {
     if (xhr.readyState !== 4) return;
-    if (xhr.status == 200) {
+    if (xhr.status === 200) {
       try {
-        var r = JSON.parse(xhr.responseText),
+        let r = JSON.parse(xhr.responseText),
           result = r["result"];
-        for (var i = 0; i < result.length; i++) {
-          var obj = r["result"][i];
+        for (let i = 0; i < result.length; i++) {
+          let obj = r["result"][i];
           output += obj.join("") + "<br/><hr/>";
         }
       } catch (e) {
       }
     }
-
     document.getElementById('info').innerHTML = output;
   }
   xhr.send()
