57293f96e86d0ca7a840fc316fe2ad02e33eb2f6
Sergey Plotnikov <37042872+SerPstrong@users.noreply.github.com>
2020-02-26 15:19:21 +0500

    Json

--- js/demontazh.js
+++ js/demontazh.js
@@ -3,18 +3,19 @@
     output = '',
     xhr = new XMLHttpRequest();
   xhr.open('GET', app);
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState !== 4) return;
     if (xhr.status == 200) {
       try {
         var r = JSON.parse(xhr.responseText),
           result = r["result"];
-        for (var i = 0; i < result.length; i++) {
+        for (var i = 0; i < result.length; i++){
           var obj = r["result"][i];
-          output += obj.join("") + "<br/><hr/>";
+          output += '<td>' + obj.join("<td/>" );
+          // output += obj.join("<br/>") + "<br/><hr/>";
+          // list += '<li>' + i + ': ' + data.items[i] + ' шт. </li>';
         }
-      } catch (e) {
-      }
+      } catch(e) {}
     }
 
     document.getElementById('info').innerHTML = output;
